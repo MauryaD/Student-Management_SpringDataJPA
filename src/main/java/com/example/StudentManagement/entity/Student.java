33b10288bb92f8926060ad94f8685d94130a0fb5
MauryaD <deepakmaurya2198@gmail.com>
2021-09-11 23:24:25 +0530

    Initial commit

---
+++ src/main/java/com/example/StudentManagement/entity/Student.java
@@ -0,0 +1,32 @@
+package com.example.StudentManagement.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "Student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long studentId;
+    private String firstName;
+    private String lastName;
+
+    @Column(name = "email_id", nullable = false)
+    private String emailId;
+    
+    private String guardianName;
+    private String guardianEmail;
+    private String guardianMobile;
+
+
+}
